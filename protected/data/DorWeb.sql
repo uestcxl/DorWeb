9eefb9835695069686c86252516a8bdf8e0ac8b6
gavin <861260447@qq.com>
2013-12-20 00:10:45 +0800

    nothing

--- protected/data/DorWeb.sql
+++ protected/data/DorWeb.sql
@@ -5,36 +5,36 @@ CREATE DATABASE DorWeb;
 USE DorWeb;
 
 -- -----------------------------------------------------
--- Table `DorWeb`.`user`
+-- Table 'DorWeb'.'user'
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `DorWeb`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `user_name` VARCHAR(20) NOT NULL ,
-  `password` VARCHAR(64) NOT NULL ,
-  PRIMARY KEY (`id`) )
+CREATE  TABLE IF NOT EXISTS 'DorWeb'.'user' (
+  'id' INT NOT NULL AUTO_INCREMENT ,
+  'user_name' VARCHAR(20) NOT NULL ,
+  'password' VARCHAR(64) NOT NULL ,
+  PRIMARY KEY ('id'))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DorWeb`.`news`
+-- Table 'DorWeb'.'news'
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `DorWeb`.`news` (
-  `news_id` INT NOT NULL AUTO_INCREMENT ,
-  `title` VARCHAR(50) NOT NULL ,
-  `content` TEXT NOT NULL ,
-  `time` DATETIME NOT NULL ,
-  PRIMARY KEY (`news_id`) )
+CREATE  TABLE IF NOT EXISTS 'DorWeb'.'news' (
+  'news_id' INT NOT NULL AUTO_INCREMENT ,
+  'title' VARCHAR(50) NOT NULL ,
+  'content' TEXT NOT NULL ,
+  'time' DATETIME NOT NULL ,
+  PRIMARY KEY ('news_id') )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `DorWeb`.`files`
+-- Table 'DorWeb'.'files'
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `DorWeb`.`files` (
-  `files_id` INT NOT NULL AUTO_INCREMENT ,
-  `file_name` VARCHAR(100) NOT NULL ,
-  `time` DATETIME NOT NULL ,
-  PRIMARY KEY (`files_id`) )
+CREATE  TABLE IF NOT EXISTS 'DorWeb'.'files' (
+  'files_id' INT NOT NULL AUTO_INCREMENT ,
+  'file_name' VARCHAR(100) NOT NULL ,
+  'time' DATETIME NOT NULL ,
+  PRIMARY KEY ('files_id') )
 ENGINE = InnoDB;
 
 
