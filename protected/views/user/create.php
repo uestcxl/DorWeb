196da9bbbf7dcc7230dbbd762a34c9fea6b12814
xulei <865492652@qq.com>
2014-03-10 18:50:34 +0800

    bb

--- protected/views/user/create.php
+++ protected/views/user/create.php
@@ -11,5 +11,51 @@
     </head>
     <div class="holder"></div>
     <center>
-    	<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+        <br>
+        <br>
+        <br>
+        <h3 style="color:#e74b3c">请 填 写 真 实 信 息</h3>
+        <br>
+        <br>
+        <form action="" method="post" name="zuche">  
+            <table>
+                <tr>
+                    <td><label>用户名称</label></td>
+                    <td><input type="text" id="user" name="User[user_name]" /></td>
+                </tr>
+                <tr>
+                    <td><label>密&nbsp&nbsp&nbsp&nbsp码</label></td>
+                    <td><input type="password" id="passwd" name="User[passwd]" /></td>
+                </tr>
+                <tr>
+                    <td><label>确认密码</label></td>
+                    <td><input type="password" id="passwd1" name="User[passwd_repeat]" /></td>
+                </tr>
+                <tr>
+                    <td><label>姓&nbsp&nbsp&nbsp&nbsp名</label></td>
+                        <td><input type="text" id="name" name="User[name]" /></td>
+                </tr>
+                <tr>
+                    <td><label>学&nbsp&nbsp&nbsp&nbsp校</label></td>
+                    <td><input type="text" id="school" name="User[school]" /></td>
+                </tr>
+                <tr>
+                    <td><label>电话号码</label></td>
+                    <td><input type="text" id="phone_num" name="User[phone]" /></td>
+                </tr>
+                <tr>
+                    <td><label>acm&nbsp_&nbspid</label></td>
+                    <td><input type="text" id="acm_id" name="User[acmid]" /></td>
+                </tr>
+                <tr>
+                    <td><label>电子邮箱</label></td>
+                    <td><input type="text" id="email" name="User[email]" /></td>
+                </tr>
+                <tr>
+                    <td>&nbsp&nbsp</td>
+                    <td>&nbsp<input type="submit" value="注册" style="width:80px;height: 25px"/></td>
+                </tr>
+            </table>
+        </form>
+
     </center>
